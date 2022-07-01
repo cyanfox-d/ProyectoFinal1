7b5b259f33518129b006b8064cab86815bebdc71
cyanfox-d <cyanfox_d@hotmail.com>
2022-06-30 20:53:31 -0600

    agregar proyecto en laravel

---
+++ resources/views/plantilla.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="esp">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Index</title>
+
+        <!-- Fonts -->
+        
+
+        <!-- Styles -->
+        <style>
+          
+        </style>
+
+        
+    </head>
+    <body>
+        
+    </body>
+</html>
